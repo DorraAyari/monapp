bf078f16b0448413fd8f63e6364359eb085491a6
dorraayaro <doura.ayari@gmail.com>
2022-02-04 14:26:14 +0100

    stripe

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -33,9 +33,12 @@
                                         <div class="p-2">
                                             <img src="{{ $product->model->image }}" alt="" width="70" class="img-fluid rounded shadow-sm">
                                             <div class="ml-3 d-inline-block align-middle">
+                                                <h5 class="mb-0"> <a  class="text-dark d-inline-block align-middle">{{ $product->model->title }}</a></h5><span class="text-muted font-weight-normal font-italic d-block">Category:</span>
                                             </div>
                                         </div>
                                     </th>
+                                    <td class="border-0 align-middle"><strong>{{ $product->model->getPrice() }}</strong></td>
+
                                     <td class="border-0 align-middle"><strong>1</strong></td>
                                     <td class="border-0 align-middle">
                                         <form action="{{ route('cart.destroy', $product->rowId) }}" method="POST">
